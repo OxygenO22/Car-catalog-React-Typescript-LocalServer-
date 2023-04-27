0a8cd39363585c437598d5712a07e66300bb69e0
OxygenO22 <bamaik@mail.ru>
2023-04-27 22:52:19 +0300

    Smile is done

--- src/components/ui/mainPage/Smile.jsx
+++ src/components/ui/mainPage/Smile.jsx
@@ -2,23 +2,21 @@ import { useState } from "react";
 import styles from "./Smile.module.scss";
 
 export const Smile = () => {
-  const [eye, setEye] = useState()
-
+  const [leftEye, setLeftEye] = useState();
+  const [rightEye, setRightEye] = useState();
+  
   function eyeball(e) {
     document.onmousemove = (e) => { 
-      let x = e.x;
-      let y = e.y;
-      console.log(x + " " + y);
-      
-
+      let x = e.x - innerWidth / 2;
+      let y = e.y - 270;
       const arcctg = (x, y) => {
-        (x > 0 && y > 0) && Math.PI / 2 - Math.atan(x / y); 
-        (x < 0 && y > 0) && Math.PI / 2 - Math.atan(x / y); 
-        (x > 0 && y < 0) && Math.PI + Math.atan(y / x); 
-        (x > 0 && y < 0) && 3 * Math.PI / 2 + Math.abs(Math.atan(y / x)); 
-      }
-
-      document.querySelector("#eye").style.transform = "rotate(" + 57.2958 * arcctg(x, y) + 'deg)';
+        if (x > 0 && y > 0) return Math.PI / 2 - Math.atan(x / y); 
+        if (x < 0 && y > 0) return Math.PI / 2 - Math.atan(x / y); 
+        if (x < 0 && y < 0) return Math.PI + Math.atan(y / x); 
+        if (x > 0 && y < 0) return 3 * Math.PI / 2 + Math.abs(Math.atan(x / y)); 
+      }  
+      setLeftEye(arcctg(x + 100, y))
+      setRightEye(arcctg(x - 100, y))
     }
   }
 
@@ -26,10 +24,35 @@ export const Smile = () => {
     <div className={styles.wrapper} onMouseMove={e => eyeball(e)}>
       <div className={styles.face}>
         <div className={styles.eyes}>
-          <div className={styles.eye} id="eye"  ></div>
-          <div className={styles.eye} ></div>
+          <div className={styles.eye} style={{transform: `rotate(${57.2958 * leftEye}deg)`}}></div>
+          <div className={styles.eye} style={{transform: `rotate(${57.2958 * rightEye}deg)`}}></div>
         </div>
       </div>
     </div>
   );
 }
+
+/* export const Smile = () => {
+  const [left, setLeft] = useState(-50);
+  const [top, setTop] = useState(-50);
+
+  function eyeball(e) {
+    document.onmousemove = (e) => { 
+      let x = e.x;
+      let y = e.y;
+      setLeft(e.x);
+      setTop(e.y);
+    }
+  }
+
+  return (
+    <div className={styles.wrapper} onMouseMove={e => eyeball(e)}>
+      <div className={styles.face}>
+        <div className={styles.eyes}>
+          <div className={styles.eye} style={{transform: `translate(${left}%, ${top}%)`}}  ></div>
+          <div className={styles.eye} ></div>
+        </div>
+      </div>
+    </div>
+  );
+} */
